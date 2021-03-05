818f24cb4deefcd7b35de5ccd393855a9db13c79
Priya2003 <priyaalq@gmail.com>
2021-03-05 10:02:54 +0100

    page connexion et inscription

---
+++ inscription.php
@@ -0,0 +1,61 @@
+<html>
+	<head>
+		<title>Inscription</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h3>Inscription</h3>
+		<br/><br/>
+			<form method="POST" action="">
+				<table>
+					<tr>
+						<td>
+							<label for="Nom">Nom :</label>
+						</td>
+						<td>
+							<input type="text" name="nom" id="nom">
+						</td>
+						<td>
+							<label for="Prénom">Prénom :</label>
+						</td>
+						<td>
+							<input type="text" name="Prénom" id="prénom">
+						</td>
+						<td>
+							<label for="Nomdutilisateur">Nom d'utilisateur :</label>
+						</td>
+						<td>
+							<input type="text" name="username" placeholder="Pseudo" id="pseudo">
+						</td>
+						<td>
+							<label for="mdp">Mot de passe :</label>
+						</td>
+						<td>
+							<input type="password" name="mdp" placeholder="Saisissez un mot de passe" id="mdp">
+						</td>
+						<td>
+							<label for="mdp2">Confirmer votre mot de passe :</label>
+						</td>
+						<td>
+							<input type="password" name="mdp2" placeholder="Saisissez à nouveau votre mot de passe" id="mdp2">
+						</td>
+						<td>
+							<label for="questionsecrète">Choisissez votre question secrète :</label>
+						</td>
+						<td>
+							<input type="text" name="mdp" placeholder="Saisissez un mot de passe" id="questionsecrète">
+						</td>
+						<td>
+							<label for="réponsequestionsecrète">Réponder à la question secrète :</label>
+						</td>
+						<td>
+							<input type="text" name="réponsequestionsecrète" placeholder="Saisissez votre réponse" id="réponsequestionsecrète">
+						</td>
+					</tr>
+				</table>
+				<input type="submit" value="Je m'inscris"/>
+				
+			</form>
+
+	</body>
+</html>
